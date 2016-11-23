65eedc97ae2ad2435b135a35507407e109d97d92
Philippe DAMOUNE <pdamoune@student.42.fr>
2016-11-23 23:19:07 +0100

    fin journee

---
+++ 1/src/ft_struct.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_struc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/22 15:36:33 by pdamoune          #+#    #+#             */
+/*   Updated: 2016/11/22 16:13:50 by pdamoune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+void	ft_struc(char *test, char *origin, unsigned char n)
+{
+	t_tetri		test;
+
+	test.nom 	= n;
+	test.test	= test;
+	test.origin	= origin;
+	test.c1	=
+	test.c2	=
+	test.c3	=
+	test.c4	=
+
+}
---
+++ 1/src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/21 08:56:03 by pdamoune          #+#    #+#             */
+/*   Updated: 2016/11/21 12:06:37 by pdamoune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_fillit(argv[1]);
+	else
+		ft_putendl("usage: ./fillit tetriminos_list_file");
+	return(0);
+}
