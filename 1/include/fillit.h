73febc0f7a893cb5ee30e435ce4883d0a52c0e8f
Philippe DAMOUNE <pdamoune@student.42.fr>
2016-11-23 23:14:53 +0100

    fin journee

---
+++ 1/include/fillit.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/21 11:19:38 by pdamoune          #+#    #+#             */
+/*   Updated: 2016/11/22 15:34:48 by pdamoune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+# include <unistd.h>
+# include <fcntl.h>
+# include "libft.h"
+
+void	ft_fillit();
+char	*ft_istetri(int fd);
+char	*ft_is_good(char *str);
+
+#endif
