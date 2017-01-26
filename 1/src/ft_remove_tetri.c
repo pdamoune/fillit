91ff4428e19358be6e15d7922cf35f310b769fe1
Philippe DAMOUNE <pdamoune@student.42.fr>
2017-01-26 12:18:17 +0100

    norme ft_remove_tetri

--- 1/src/ft_remove_tetri.c
+++ 1/src/ft_remove_tetri.c
@@ -6,15 +6,11 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 08:45:30 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/01/23 08:19:44 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/01/26 12:17:46 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fillit.h"
-
-/*
-** Remove the current *tetri from the string *result.
-*/
 
 void	ft_remove_tetri(char *tetri, char *result)
 {
@@ -28,10 +24,6 @@ void	ft_remove_tetri(char *tetri, char *result)
 	while (result[i] && (result[i] != tetri[j]))
 		i++;
 	while (result[i])
-	{
-		if (result[i] == tetri[j])
-			result[i] = '.';
-		i++;
-		j++;
-	}
+		if (result[i] == tetri[j++])
+			result[i++] = '.';
 }
