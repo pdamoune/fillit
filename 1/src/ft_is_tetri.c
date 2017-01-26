07530214817b15869b00eb2ba1bf723c7c7fa445
Philippe DAMOUNE <pdamoune@student.42.fr>
2017-01-26 12:24:32 +0100

    norme ok, manque que le parsing

--- 1/src/ft_is_tetri.c
+++ 1/src/ft_is_tetri.c
@@ -6,16 +6,27 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 12:40:29 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/01/24 14:06:26 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/01/26 12:22:55 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fillit.h"
 #include <stdio.h>
 
-char 	*ft_set_tetris(char *buf)
+void	ft_move_top(char *buf)
 {
-	char *tetri;
+	while (buf[0] == '.' && buf[4] == '.' && buf[8] == '.'
+	&& buf[12] != '#')
+		ft_strcpy(buf, buf + 1);
+	while (buf[0] == '.' && buf[1] == '.' && buf[2] == '.'
+	&& buf[3] == '.')
+		ft_strcpy(buf, &buf[4]);
+	*(ft_strrchr(buf, '#') + 1) = 0;
+}
+
+char	*ft_set_tetris(char *buf)
+{
+	char	*tetri;
 
 	tetri = ft_strnew(200);
 	ft_bzero(tetri, 201);
@@ -25,11 +36,11 @@ char 	*ft_set_tetris(char *buf)
 	return (tetri);
 }
 
-int 	ft_is_tetri(int fd, char **tetris, char **lst_tetri)
+int		ft_is_tetri(int fd, char **tetris, char **lst_tetri)
 {
-	char			buf[21];
-	int				next;
-	int				letter;
+	char	buf[21];
+	int		next;
+	int		letter;
 
 	letter = 'A';
 	ft_bzero(buf, 22);
@@ -39,13 +50,7 @@ int 	ft_is_tetri(int fd, char **tetris, char **lst_tetri)
 		ft_strlen(buf) == 20 ? next = 0 : 1;
 		if (!ft_parsing(buf))
 			return (0);
-		while (buf[0] == '.' && buf[4] == '.' && buf[8] == '.'
-			&& buf[12] != '#')
-				ft_strcpy(buf, buf + 1);
-		while (buf[0] == '.' && buf[1] == '.' && buf[2] == '.'
-			&& buf[3] == '.')
-				ft_strcpy(buf, &buf[4]);
-		*(ft_strrchr(buf, '#') + 1) = 0;
+		ft_move_top(buf);
 		while (ft_strchr(buf, '#'))
 			*ft_strchr(buf, '#') = letter;
 		tetris[letter - 65] = ft_set_tetris(buf);
