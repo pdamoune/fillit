9e84e91de3a3d6fd9b240a5e49ea51e9672c5f6f
Philippe DAMOUNE <pdamoune@student.42.fr>
2017-01-26 10:52:48 +0100

    norme ft_set_square

--- 1/src/ft_set_square.c
+++ 1/src/ft_set_square.c
@@ -6,20 +6,15 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 08:52:02 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/01/25 18:01:01 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/01/26 10:51:35 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fillit.h"
 
-/*
-** Modify all the tetris from **tetris and **lst_tetri, to fit in a
-** sqr_min square.
-*/
-
-int 	ft_set_square(char **tetris, char **lst_tetri, char *result, int sqr_min)
+int		ft_set(char **tetris, char **lst_tetri, char *result, int sqr_min)
 {
-	int i;
+	int	i;
 
 	i = 4;
 	while ((int)ft_strlen(tetris[0]) < sqr_min)
@@ -28,13 +23,13 @@ int 	ft_set_square(char **tetris, char **lst_tetri, char *result, int sqr_min)
 		i++;
 	}
 	if (sqr_min == 9)
-	 	if (!ft_lower_square(tetris, result, sqr_min))
+		if (!ft_lower_square(tetris, result, sqr_min))
 		{
 			ft_memset(result, '.', 16);
 			result[16] = 0;
-			return (0);
+			return (16);
 		}
 	ft_memset(result, '.', sqr_min);
 	result[sqr_min] = 0;
-	return (1);
+	return (sqr_min);
 }
