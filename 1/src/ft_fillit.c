c76aa13304af79b52cc97ebfa206ba859c5d9880
Philippe DAMOUNE <pdamoune@student.42.fr>
2017-01-26 10:42:02 +0100

    norme ft_fillit

--- 1/src/ft_fillit.c
+++ 1/src/ft_fillit.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 10:57:50 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/01/24 16:57:48 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/01/26 10:38:36 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,21 @@
 void	ft_fillit(char *argv)
 {
 	int		fd;
-	int		sqrt_min;
+	int		sqr_min;
 	char	*lst_tetri[27];
 	char	*tetris[27];
-
-	// char	*tetri;
+	char	result[200];
 
 	ft_bzero(lst_tetri, 28);
 	ft_bzero(tetris, 28);
+	ft_bzero(result, 201);
+	ft_memset(result, '.', 196);
 	fd = open(argv, O_RDONLY);
-    if (fd == -1)
+	if (fd == -1)
 		ft_putendl("error");
-	if ((sqrt_min = ft_is_tetri(fd, tetris, lst_tetri)) == 0)
+	if ((sqr_min = ft_is_tetri(fd, tetris, lst_tetri)) == 0)
 		ft_putendl("error");
 	else
-	{
-		sqrt_min *= sqrt_min;
-		if (!ft_strcmp(lst_tetri[0], "AA..AA") && sqrt_min == 4)
-			ft_strcpy(tetris[0], "AAAA");
-		else if(sqrt_min == 4)
-			sqrt_min = 9;
-		ft_solver(tetris, lst_tetri, sqrt_min);
-	}
-	// printf("grille : %d\n\n", sqrt_min);
+		ft_solver(tetris, lst_tetri, result, sqr_min * sqr_min);
 	close(fd);
-
 }
