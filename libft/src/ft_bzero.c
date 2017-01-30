5e24a686b3ccf8a49ceb8352befa994d102dd5ba
Philippe DAMOUNE <pdamoune@student.42.fr>
2017-01-30 17:34:27 +0100

    libft

---
+++ libft/src/ft_bzero.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/08 13:46:21 by pdamoune          #+#    #+#             */
+/*   Updated: 2017/01/17 16:22:19 by pdamoune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Sets the first n bytes of the area starting at s to zero (bytes
+** containing '\0').
+*/
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+		str[i++] = '\0';
+}
